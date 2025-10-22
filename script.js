df56f99833275c46395272718592aa973157f6e7
Rémi RUSSI <remi.russi@ecole-isitech.fr>
2025-10-22 18:45:09 +0200

    Ajout texte descriptif module + logique JS pour affichage des cards

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ const buttons = document.querySelectorAll('.btnInfoCard')
 buttons.forEach(btn => {
     btn.addEventListener('click', (e) => {
         const clickedCard = e.target.closest('.card');
+        const isActiveCard = clickedCard.classList.contains('move');
+        const clickedText = clickedCard.querySelector('.cardInfo');
 
         document.querySelectorAll('.card').forEach(card => {
         card.classList.remove('move');
+        card.querySelector('.cardInfo').classList.remove('textAppear');
         });
 
+        if (!isActiveCard){
         clickedCard.classList.add('move');
+        clickedText.classList.add('textAppear');
+        }
+
     });
 });
